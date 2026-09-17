55b21fcf2bd1ab530a944c0ac88cd7934d88c3e3
paban <pabankandel2@gmail.com>
2026-09-17 10:52:45 +0545

    add the language switcher and error fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import GoogleTranslate from "../components/GoogleTranslate";
 import Interactions from "../components/Interactions";
 import "./globals.css";
 export const metadata: Metadata = {
@@ -53,6 +54,7 @@ export default function RootLayout({
         <main id="main">{children}</main>
         <Footer />
         <Interactions />
+        <GoogleTranslate />
       </body>
     </html>
   );
